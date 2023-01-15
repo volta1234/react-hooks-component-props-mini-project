94540415bc41f2d53d46f2aa27c3e1a4ea216d14
CALVINCE <calvince.ogwel@gmail.com>
2023-01-15 09:08:32 +0300

    completed compulsory lab

---
+++ src/components/ArticleList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Article from "./Article";
+
+function Articlelist({posts}) {
+    return(
+        <main>
+            {posts.map((post)=> <Article key={post.id} title={post.title} date={post.date} preview={post.preview} /> )}
+        </main>
+    )
+}
+
+export default Articlelist;
